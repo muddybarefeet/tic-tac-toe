a868e9e30f857f7bc5993b62750c838d06d73152
Anna Rogers <2muddybarefeet@gmail.com>
2016-10-18 17:22:51 -0700

    add basic board functionality

--- src/App.js
+++ src/App.js
@@ -23,12 +23,30 @@ class App extends Component {
     }
   }
 
+  handleClick (row,cell) {
+    var currentTurn = this.state.player;
+    var currentNumberOfTurns = this.state.turnCount;
+
+    var newBoard = this.state.board.slice();
+    newBoard[row][cell] = currentTurn;
+
+    this.setState({
+      board: newBoard,
+      player: currentTurn === NAUGHT ? CROSS : NAUGHT,
+      turnCount: currentNumberOfTurns+=1
+    });
+
+  }
+
   render() {
 
-    var count = 0;
+    var finished;
+    
+    if (this.state.turnCount === 9) finished = "Game Over";
+
     var board = this.state.board.map( (row, index) => {
-      var newRow = row.map(function (cell, i) {
-        return (<td key={i} style={{padding:"40px"}}>{cell}</td>);
+      var newRow = row.map( (cell, i) => {
+        return (<td key={i} style={{padding:"40px"}} onClick={this.handleClick.bind(this,index,i)}>{cell}</td>);
       });
       return (<tr key={index}>{newRow}</tr>);
     });
@@ -36,7 +54,7 @@ class App extends Component {
     return (
       <div className="container">
         <h1>This is a game of Tic Tac Toe</h1>
-
+        {this.state.turnCount === 9 ? <p>Game over</p> : null}
         <table className="table-bordered">
           <tbody>
             {board}
